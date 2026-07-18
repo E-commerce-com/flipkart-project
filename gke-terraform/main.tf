56cadd7d7ff48af83b196fec4d1ecc2ca01e5492
Rakeshbandi9596 <bandirakesh.info2026@gmail.com>
2026-07-18 17:11:00 +0530

    Refactor GKE cluster and node pool configuration
    Updated GKE cluster configuration to use 'var.zone' instead of 'var.region' and improved comments for clarity.

--- gke-terraform/main.tf
+++ gke-terraform/main.tf
@@ -22,7 +22,7 @@ resource "google_compute_firewall" "allow-internal" {
   source_ranges = ["10.10.0.0/16"]
 }
 
-# Firewall-2 for External Access SSH, icmp, RDP
+# Firewall-2 for External Access (SSH, ICMP, RDP)
 resource "google_compute_firewall" "allow-external" {
   name    = "external-firewall"
   network = google_compute_network.custom_network.id
@@ -52,29 +52,38 @@ resource "google_compute_firewall" "allow-gke" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-
 # GKE Cluster
 resource "google_container_cluster" "primary" {
-  project             = var.project
-  name                = "terraform-gke-cluster"
-  location            = var.region
-  network             = google_compute_network.custom_network.id
-  subnetwork          = google_compute_subnetwork.custom-subnet.id
-  deletion_protection = false
+  project    = var.project
+  name       = "terraform-gke-cluster"
+  location   = var.zone
+  network    = google_compute_network.custom_network.id
+  subnetwork = google_compute_subnetwork.custom-subnet.id
+
+  deletion_protection      = false
   remove_default_node_pool = true
-  initial_node_count       = 1  
+  initial_node_count       = 1
+
+  min_master_version = var.K8s_version
 }
 
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name           = "my-node-pool"
-  cluster        = google_container_cluster.primary.name
-  location       = google_container_cluster.primary.location
-  node_count     = 1  
+# Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name     = "my-node-pool"
+  project  = var.project
+  cluster  = google_container_cluster.primary.name
+  location = var.zone
+
+  node_count = 1
 
   node_config {
-    machine_type   = "e2-medium"  # Use a smaller instance to reduce storage requirements
-    disk_size_gb   = "15"
-    disk_type      = "pd-standard"
-    image_type     = "UBUNTU_CONTAINERD"
+    machine_type = "e2-medium"
+    disk_size_gb = 15
+    disk_type    = "pd-standard"
+    image_type   = "UBUNTU_CONTAINERD"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
 }
