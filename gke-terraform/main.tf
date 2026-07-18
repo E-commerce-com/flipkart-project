abb2b59dda9c0010be65f3783bc23d8e7bc6498f
Rakeshbandi9596 <bandirakesh.info2026@gmail.com>
2026-07-18 17:18:39 +0530

    Refactor GKE Terraform configuration for clarity

--- gke-terraform/main.tf
+++ gke-terraform/main.tf
@@ -1,17 +1,56 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+
+# Enable Required APIs
+
+resource "google_project_service" "container_api" {
+  project = var.project
+  service = "container.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "compute_api" {
+  project = var.project
+  service = "compute.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam_api" {
+  project = var.project
+  service = "iam.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+
+# VPC Network
+
 resource "google_compute_network" "custom_network" {
   name                    = "custom-vpc"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "custom-subnet" {
+
+# Subnet
+
+resource "google_compute_subnetwork" "custom_subnet" {
   name          = "custom-subnetwork"
   ip_cidr_range = "10.10.0.0/16"
   region        = var.region
   network       = google_compute_network.custom_network.id
 }
 
-# Firewall-1 for Internal Communication
-resource "google_compute_firewall" "allow-internal" {
+
+# Internal Firewall
+
+resource "google_compute_firewall" "allow_internal" {
+
   name    = "internal-firewall"
   network = google_compute_network.custom_network.id
 
@@ -19,11 +58,16 @@ resource "google_compute_firewall" "allow-internal" {
     protocol = "all"
   }
 
-  source_ranges = ["10.10.0.0/16"]
+  source_ranges = [
+    "10.10.0.0/16"
+  ]
 }
 
-# Firewall-2 for External Access (SSH, ICMP, RDP)
-resource "google_compute_firewall" "allow-external" {
+
+# External Firewall
+
+resource "google_compute_firewall" "allow_external" {
+
   name    = "external-firewall"
   network = google_compute_network.custom_network.id
 
@@ -33,57 +77,114 @@ resource "google_compute_firewall" "allow-external" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "3389"]
+    ports = [
+      "22",
+      "3389"
+    ]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
 }
 
-# Firewall-3 for GKE Communication
-resource "google_compute_firewall" "allow-gke" {
+
+# GKE Firewall
+
+resource "google_compute_firewall" "allow_gke" {
+
   name    = "gke-firewall"
   network = google_compute_network.custom_network.id
 
   allow {
     protocol = "tcp"
-    ports    = ["443", "10250", "15017"]
+
+    ports = [
+      "443",
+      "10250",
+      "15017"
+    ]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
 }
+
 
 # GKE Cluster
+
 resource "google_container_cluster" "primary" {
-  project    = var.project
-  name       = "terraform-gke-cluster"
-  location   = var.zone
-  network    = google_compute_network.custom_network.id
-  subnetwork = google_compute_subnetwork.custom-subnet.id
 
-  deletion_protection      = false
+  depends_on = [
+    google_project_service.container_api,
+    google_project_service.compute_api
+  ]
+
+  name     = "terraform-gke-cluster"
+
+  project  = var.project
+
+  location = var.zone
+
+  network = google_compute_network.custom_network.id
+
+  subnetwork = google_compute_subnetwork.custom_subnet.id
+
+
+  min_master_version = var.k8s_version
+
+
+  deletion_protection = false
+
+
   remove_default_node_pool = true
-  initial_node_count       = 1
 
-  min_master_version = var.K8s_version
+  initial_node_count = 1
 }
 
+
+
 # Node Pool
+
 resource "google_container_node_pool" "primary_nodes" {
-  name     = "my-node-pool"
-  project  = var.project
-  cluster  = google_container_cluster.primary.name
+
+  depends_on = [
+    google_container_cluster.primary
+  ]
+
+
+  name = "my-node-pool"
+
+
+  project = var.project
+
+
+  cluster = google_container_cluster.primary.name
+
+
   location = var.zone
+
 
   node_count = 1
 
+
+
   node_config {
+
     machine_type = "e2-medium"
+
     disk_size_gb = 15
-    disk_type    = "pd-standard"
-    image_type   = "UBUNTU_CONTAINERD"
+
+    disk_type = "pd-standard"
+
+    image_type = "UBUNTU_CONTAINERD"
+
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
+
   }
+
 }
