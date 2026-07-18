93d6f39a0a869e9c1a1978921446386ce1c4ebd9
Rakeshbandi9596 <bandirakesh.info2026@gmail.com>
2026-07-18 17:18:04 +0530

    Update variable.tf

--- gke-terraform/variable.tf
+++ gke-terraform/variable.tf
@@ -1,23 +1,23 @@
 variable "project" {
-  description = "Google Cloud Project ID"
+  description = "GCP project ID"
   type        = string
   default     = "project-73bd9651-4490-4776-91a"
 }
 
 variable "region" {
-  description = "Google Cloud Region"
+  description = "GCP region"
   type        = string
   default     = "asia-south1"
 }
 
 variable "zone" {
-  description = "Google Cloud Zone"
+  description = "GCP zone"
   type        = string
   default     = "asia-south1-a"
 }
 
-variable "K8s_version" {
-  description = "GKE Kubernetes Version"
+variable "k8s_version" {
+  description = "GKE Kubernetes version"
   type        = string
   default     = "1.31.6-gke.1020000"
 }
